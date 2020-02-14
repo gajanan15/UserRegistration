4baba6bed3f03bcc20dce1b3deefd623ac2a204c
gajanan15 <gajanandesai97@gmail.com>
2020-02-14 11:07:20 +0530

    ADDED: Validation Of Last Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,11 +2,11 @@
 shopt -s extglob
 echo "Welcome To User Registration Form"
 
-echo "Enter User Name: "
-read firstName
+echo "Enter User First Name And Last Name: "
+read firstName lastName
 validPattern="^[A-Z]{1}[a-z]{2,}$"
 
-if [[ $firstName =~ $validPattern ]]
+if [[ $firstName =~ $validPattern && $lastName =~ $validPattern ]]
 then
 	echo "Valid"
 else
