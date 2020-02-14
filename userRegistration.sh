a675390a22f6f59c092fdc03c758c427773c3132
gajanan15 <gajanandesai97@gmail.com>
2020-02-14 16:03:51 +0530

    ADDED: Validation Of Mobile Number

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,8 +2,6 @@
 shopt -s extglob
 echo "Welcome To User Registration Form"
 
-
-
 function validPatternCheck() {
 	userInput=$1
 	checkPattern=$2
@@ -17,11 +15,19 @@ function validPatternCheck() {
 
 echo "Enter User First Name And Last Name:"
 read firstName lastName
+
 validNamePattern="^[A-Z]{1}[a-z]{2,}$"
-validPatternCheck $firstName $validNamePattern
-validPatternCheck $lastName $validNamePattern
+validPatternCheck "$firstName" $validNamePattern
+validPatternCheck "$lastName" $validNamePattern
 
 echo "Enter Email Id:"
 read email
+
 emailPattern="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
-validPatternCheck $email $emailPattern
+validPatternCheck "$email" $emailPattern
+
+echo "Enter Mobile No:"
+read mobileNumber
+
+validNumberPattern="^[0-9]{1,3}[[:space:]][0-9]{10}$"
+validPatternCheck "$mobileNumber" $validNumberPattern
