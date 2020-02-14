f0d9e91de57778ce2bd7b54c5c35a0319275bec6
gajanan15 <gajanandesai97@gmail.com>
2020-02-14 10:50:20 +0530

    ADDED: Validation Of First Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,14 @@
 #!/bin/bash -x
+shopt -s extglob
 echo "Welcome To User Registration Form"
+
+echo "Enter User Name: "
+read firstName
+validPattern="^[A-Z]{1}[a-z]{2,}$"
+
+if [[ $firstName =~ $validPattern ]]
+then
+	echo "Valid"
+else
+	echo "Invalid"
+fi
